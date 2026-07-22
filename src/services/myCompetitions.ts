f3c9ce28492b5249bc526d9037490d3d80dc0f3c
g0595942422-boop <g0595942422@gmail.com>
2026-07-22 16:26:16 +0800

    Update myCompetitions.ts

--- src/services/myCompetitions.ts
+++ src/services/myCompetitions.ts
@@ -1,7 +1,6 @@
-import { Competition } from "./competitions";
+import { Competition } from './competitions';
 
-const STORAGE_KEY = "myCompetitions";
-
+const STORAGE_KEY = 'myCompetitions';
 
 export function getMyCompetitions(): Competition[] {
   const data = localStorage.getItem(STORAGE_KEY);
@@ -17,17 +16,23 @@ export function getMyCompetitions(): Competition[] {
 export function addMyCompetition(
   competition: Competition
 ) {
+
   const list = getMyCompetitions();
 
   const exists = list.some(
     item => item.id === competition.id
   );
 
+
   if (!exists) {
     list.push(competition);
+
     localStorage.setItem(
       STORAGE_KEY,
       JSON.stringify(list)
     );
   }
+
+
+  return !exists;
 }
