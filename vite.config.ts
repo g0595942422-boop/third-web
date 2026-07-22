fd5172a175898a9402cf084097f85c8be99c471c
g0595942422-boop <g0595942422@gmail.com>
2026-07-22 13:56:22 +0800

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/third-web/',
+  base: "/third-web/",
   plugins: [react()],
 })
